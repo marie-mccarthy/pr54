1a7ef1c88d5aa1720dd8f5ceed833dc9ecd7eefb
Marie McCarthy <marie.mccarthy2000@gmail.com>
2016-12-07 10:09:31 -0700

    working on it... I am not sure how to print it, but i think it will work soon .

--- pr54/src/pr54/pr54.java
+++ pr54/src/pr54/pr54.java
@@ -1,14 +1,13 @@
 package pr54;
 
-import Dog;
-
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class pr54 {
-public static void main(String[] args)
+public static void main(String[] args) throws FileNotFoundException
 {
 	ArrayList <Student> Class = new ArrayList <Student>();
   Scanner file = new Scanner(new File( "pr54.dat" ));
@@ -23,35 +22,31 @@ public static void main(String[] args)
 		Class.add(new Student(teacher, student));
 file.nextLine();
     //sorts the array
-//    for (int j = 0; j < (home.getTeacher()).legnth-1; j++)
+    for (int j = 0; j < (Class.get(zz).getTeacher()).length()-1; j++)
+
 //    {
 //      int minIndex = j;
-//      for (int k = j + 1; k < home.size(); k++)
+//      for (int k = j + 1; k < Class.size(); k++)
 //      {
-//        if (home.get(k).getList() < home.get(k).getList())
+//        if (Class.get(k).getList() < Class.get(k).getList())
 //        {
 //        minIndex = k;
 //        }
 //      }
-//      list temp =  home.get(j);
-//      home.get(j) = home.get(minIndex);
-//      home.get(minIndex) = temp;
+//      list temp =  Class.get(j);
+//      home.get(j) = Class.get(minIndex);
+//      Class.get(minIndex) = temp;
 //    //removes duplicates
-//  if(home.contains(home.get(zz)))
-//        {
-//        home.remove(zz);
-//        }
-//        //removes spaces
-//    if(home.contains(" "))
-//        {
-//        home.remove(" ");
-//        }
-//  }
-//  //print the array
-//    for(int i= 0 ; i <home.size(); i ++)
-//    {
-//      System.out.println(home.get(i));
- //   }
+    	
+  //print the array
+    for(int i= 0 ; i <Class.size(); i ++)
+    {
+      System.out.println(Class.get(i).getTeacher());     
+      if(Class.get(i).getName().equals(Class.get(i).getTeacher()))
+    	  {
+    		  System.out.println(Class.get(i).getName());
+    	  }
+    }
 }
 }
 
